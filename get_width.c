71af72be0b624f8a55bda99aec8603bc1f9027e1
AbiollaghJames <abiollajames@protonmail.com>
2023-01-26 11:55:24 -0800

    get width

--- get_width.c
+++ get_width.c
@@ -1,14 +1,17 @@
 #include "main.h"
+
 /**
- * get_width - calculates the width for printing
- * @format: formatted string to print
- * @i: list of args to be printed
- * @list: list of args
- * Return: width
+ * get_width - Calculates the width for printing
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: width.
  */
 int get_width(const char *format, int *i, va_list list)
 {
-	int curr_i, width = 0;
+	int curr_i;
+	int width = 0;
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
@@ -26,6 +29,7 @@ int get_width(const char *format, int *i, va_list list)
 		else
 			break;
 	}
+
 	*i = curr_i - 1;
 
 	return (width);
