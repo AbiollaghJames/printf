c2740533de2e3fb54f26c29375e2315c834ac0f6
AbiollaghJames <abiollajames@protonmail.com>
2023-01-26 10:33:51 -0800

    handle print

--- handle_print.c
+++ handle_print.c
@@ -20,7 +20,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+		{'r', print_sreverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
